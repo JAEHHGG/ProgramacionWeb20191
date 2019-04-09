1aad4d536e619c5d4286805af4100f1b2766414a
Tu Nombre <you@example.com>
2019-04-09 07:45:59 -0500

    Add formulario de registro y agregado respuesta

---
+++ Semana10/respuesta.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Respuesta</title>
+</head>
+<body>
+    <?php
+
+    echo $GET['usser'];
+    echo '<br>'
+    echo $GET['pass'];
+
+    ?>
+</body>
+</html>
