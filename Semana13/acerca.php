a498c42f15328d0d8f663867066ad307ba11dc73
Tu Nombre <you@example.com>
2019-04-30 08:06:45 -0500

    Mejorada la navegación, agregado acerca de

---
+++ Semana13/acerca.php
@@ -0,0 +1,11 @@
+<?php include('templates/header.php'); ?>
+
+<div class="main">
+    <h2>Accerca de</h2>
+
+    <section>esta es la persona 1 del equipo</section>
+    <section>esta es la persona 2 del equipo</section>
+    <section>esta es la persona 3 del equipo</section>
+</div>
+
+<?php include('templates/footer.php'); ?>
