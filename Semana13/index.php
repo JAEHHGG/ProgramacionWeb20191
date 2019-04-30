40b72fa72fc623e0e276fb95d1f2a441190356ec
Tu Nombre <you@example.com>
2019-04-30 07:49:19 -0500

    Arreglado bug str en las rutas

--- Semana13/index.php
+++ Semana13/index.php
@@ -1,4 +1,4 @@
-<?php include(templates/header.php); ?>
+<?php include('templates/header.php'); ?>
 
 <div class="main">
     <h2>Este es el contenido principal</h2>
@@ -6,4 +6,4 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea mollitia cumque culpa, sapiente inventore nulla ipsum dicta eaque blanditiis, dolorum tempora ab facere. Molestias quasi corporis, recusandae rerum fuga eligendi impedit unde atque, fugit illum eum? Ratione, quaerat velit culpa a ipsum nihil distinctio quisquam. Maiores cumque mollitia pariatur ex amet eius odio illum beatae ut corporis possimus fugit cum, unde impedit alias neque aspernatur adipisci et quae corrupti dolores. Odio, numquam, molestias! Consectetur dolorem esse, deleniti quasi in accusantium consequatur dicta quae aspernatur natus. Tenetur, non, repellendus. Fugiat soluta, mollitia expedita at consequuntur illum similique recusandae quidem magnam, temporibus.</p>
 </div>
 
-<?php include(templates/footer.php); ?>
+<?php include('templates/footer.php'); ?>
