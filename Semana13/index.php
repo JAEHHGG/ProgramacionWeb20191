b17e8637c5d63283e617f5b1ba77cebfd01d3b4f
Tu Nombre <you@example.com>
2019-04-30 07:47:57 -0500

    Uso de include

--- Semana13/index.php
+++ Semana13/index.php
@@ -1,50 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Mi asombroso sitio</title>
-    <link rel="stylesheet" href="estilos/main.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-
-   <header>
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-   </header>
+<?php include(templates/header.php); ?>
 
 <div class="main">
     <h2>Este es el contenido principal</h2>
@@ -52,12 +6,4 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea mollitia cumque culpa, sapiente inventore nulla ipsum dicta eaque blanditiis, dolorum tempora ab facere. Molestias quasi corporis, recusandae rerum fuga eligendi impedit unde atque, fugit illum eum? Ratione, quaerat velit culpa a ipsum nihil distinctio quisquam. Maiores cumque mollitia pariatur ex amet eius odio illum beatae ut corporis possimus fugit cum, unde impedit alias neque aspernatur adipisci et quae corrupti dolores. Odio, numquam, molestias! Consectetur dolorem esse, deleniti quasi in accusantium consequatur dicta quae aspernatur natus. Tenetur, non, repellendus. Fugiat soluta, mollitia expedita at consequuntur illum similique recusandae quidem magnam, temporibus.</p>
 </div>
 
-   <aside></aside>
-
-   <footer>
-       Hecho con <i>&#128153;</i> para mi clase de desarrollo web
-   </footer>
-
-   <script type="text/javascript" src="js/main.js"></script>
-</body>
-</html>
+<?php include(templates/footer.php); ?>
---
+++ Semana13/templates/footer.php
@@ -0,0 +1,12 @@
+<aside>
+       Esta será mi barra lateral
+
+   </aside>
+
+   <footer>
+       Hecho con <i>&#128153;</i> para mi clase de desarrollo web
+   </footer>
+
+   <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
