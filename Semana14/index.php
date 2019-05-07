741195cad6554c2ece758370335d87e540cdd0b2
Tu Nombre <you@example.com>
2019-05-07 08:51:26 -0500

    Front

---
+++ Semana14/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Selecciones de usuario</title>
+</head>
+<body>
+    <form action="seleccionado.php" method="get">
+        <input type="text" name="user" id="user" placeholder="Usuario">
+        <select name="seleccion" id="seleccion">
+            <option value="opcion1">"opcion1"</option>
+            <option value="opcion2">"opcion2"</option>
+            <option value="opcion3">"opcion3"</option>
+            <option value="opcion4">"opcion4"</option>
+            <option value="opcion5">"opcion5"</option>
+        </select>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
