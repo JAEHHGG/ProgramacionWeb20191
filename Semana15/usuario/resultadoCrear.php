8200d30b2b3a457fd750c0775411ffbf2924984f
Tu Nombre <you@example.com>
2019-05-14 09:02:00 -0500

    Add resultadoCrear.php

---
+++ Semana15/usuario/resultadoCrear.php
@@ -0,0 +1,16 @@
+<?php include('../templates/header.php'); ?>
+
+<?php
+
+    if (isset($_POST['nombre'])){
+        echo '<br>'$_POST['nombre'];
+    }
+
+    if (isset($_POST['apellido'])){
+        echo '<br>'$_POST['apellido'];
+    }
+
+?>
+
+
+<?php include('../templates/footer.php'); ?>
