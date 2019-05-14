37ae139ace265cf67670b1efb7decd82dedf8f99
Tu Nombre <you@example.com>
2019-05-14 08:58:48 -0500

    Improved form

--- Semana15/usuario/crear.php
+++ Semana15/usuario/crear.php
@@ -1,10 +1,10 @@
 <?php include('../templates/header.php'); ?>
 <div class="container">
 <form class="form-group" action="resultadoCrear.php" method="post">
-    <input class="form-control" type="text" name="nombre" id="nomre">
-    <input class="form-control" type="text" name="apellido" id="apellido">
-    <input class="form-control" type="email" name="email" id="email">
-    <input class="form-control" type="password" name="pass" id="pass">
+    <input class="form-control" type="text" name="nombre" id="nomre" placeholder="&#128100; Nombre">
+    <input class="form-control" type="text" name="apellido" id="apellido" placeholder="&#128100; Apellido">
+    <input class="form-control" type="email" name="email" id="email" placeholder="&#128231;  Correo">
+    <input class="form-control" type="password" name="pass" id="pass" placeholder="&#128273; Contraseña">
     <input class="btn btn-primary" type="submit" value="Crear Usuario">
 </form>
 </div>
