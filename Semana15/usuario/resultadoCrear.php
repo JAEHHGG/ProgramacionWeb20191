9a685ab4923e92a3b23d89d5f8e9a1f666b6d02c
Tu Nombre <you@example.com>
2019-05-16 08:15:25 -0500

    Arreglado falta de ;

--- Semana15/usuario/resultadoCrear.php
+++ Semana15/usuario/resultadoCrear.php
@@ -11,11 +11,11 @@ include('../phpresources/dbConection.php');
 <?php
 
     if (isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['email']) && isset($_POST['pass'])){
-        $sentenciaSQL = "INSERT INTO `usuario` ( `nombre`, `apellido`, `email`, `pass`, `activo`) "
-        $sentenciaSQL += "VALUES ( '"
-        $sentenciaSQL += $_POST['nombre']."', '"
-        $sentenciaSQL += $_POST['apellido']"', '"
-        $sentenciaSQL += $_POST['email']"', '"
+        $sentenciaSQL = "INSERT INTO `usuario` ( `nombre`, `apellido`, `email`, `pass`, `activo`) ";
+        $sentenciaSQL += "VALUES ( '";
+        $sentenciaSQL += $_POST['nombre']."', '";
+        $sentenciaSQL += $_POST['apellido']"', '";
+        $sentenciaSQL += $_POST['email']"', '";
         $sentenciaSQL += $_POST['pass']"', 1)";
 
         echo $sentenciaSQL;
