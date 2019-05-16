b282d38014935f74b8b696284c1660e7ad89cf0d
Tu Nombre <you@example.com>
2019-05-16 08:20:06 -0500

    sentencia v3

--- Semana15/usuario/resultadoCrear.php
+++ Semana15/usuario/resultadoCrear.php
@@ -12,7 +12,7 @@ include('../phpresources/dbConection.php');
 
     if (isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['email']) && isset($_POST['pass'])){
         $sentenciaSQL = "INSERT INTO `usuario` ( `nombre`, `apellido`, `email`, `pass`, `activo`) ";
-        $sentenciaSQL += "VALUES ( '";
+        $sentenciaSQL = $sentenciaSQL."VALUES ( '";
         /*$sentenciaSQL += $_POST['nombre']."', '";
         $sentenciaSQL += $_POST['apellido']"', '";
         $sentenciaSQL += $_POST['email']"', '";
